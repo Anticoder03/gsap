55be28fe2ae77574992d4ccea4604169aa9d3380
Anticoder03 <ap5381545@gmail.com>
2024-12-12 15:53:17 +0530

    day-4

---
+++ day-4/script.js
@@ -0,0 +1,28 @@
+var main = document.querySelector("#main");
+var cursor = document.querySelector("#cursor");
+var imgDiv = document.querySelector("#image");
+main.addEventListener("mousemove",function (dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+        duration:1,
+        ease:"power2.out"
+    })
+})
+
+imgDiv.addEventListener("mouseenter",function (dets){
+    cursor.innerHTML = "View More";
+
+    gsap.to(cursor,{
+        scale:2,
+        backgroundColor:"#e9e9e957",
+        color:"#fff"
+    })
+})
+imgDiv.addEventListener("mouseleave",function (dets){
+    gsap.to(cursor,{
+        scale:1,
+        backgroundColor:"#fff",
+        color:"#000"
+    })
+})
